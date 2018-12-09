1e572af0ee94bf3877753de711247f82f1005597
omarcsejust <omarhasan951@gmail.com>
2018-12-09 21:07:32 +0600

    added calculation for finding D

--- src/ChaumBlindSignature.java
+++ src/ChaumBlindSignature.java
@@ -6,6 +6,7 @@ public class ChaumBlindSignature {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		Helper helper = new Helper();
 		
 		System.out.println("Input 1st prime number");
 		BigInteger p = scanner.nextBigInteger();
@@ -16,7 +17,15 @@ public class ChaumBlindSignature {
 		BigInteger n = p.multiply(q);
 		
 		BigInteger fiOfn = p.subtract(new BigInteger("1")).multiply(q.subtract(new BigInteger("1")));
-		System.out.println(fiOfn);
+		
+		System.out.println("Choose an integer");
+		int e = scanner.nextInt();
+		
+		BigInteger data = helper.calculateD(fiOfn, e);
+		
+		System.out.println(data);
+		
+		
 		
 	}
 
--- src/Helper.java
+++ src/Helper.java
@@ -1,4 +1,12 @@
+import java.math.BigInteger;
 
 public class Helper {
+	
+	public BigInteger calculateD(BigInteger fiOfn, int e) {
+		BigInteger d = null;
+		BigInteger product = fiOfn.multiply(new BigInteger("2")).add(new BigInteger("1"));
+		d = product.divide(new BigInteger(Integer.toString(e)));
+		return d;
+	}
 
 }
